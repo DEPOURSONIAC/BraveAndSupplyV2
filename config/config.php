49e1627c34abd4074868e79a139cc1bc9c35c979
DEPOUROSNIAC <DEPOURSONIAC.JEAN@outlook.com>
2026-08-14 18:31:38 +0200

    Add files via upload

--- config/config.php
+++ config/config.php
@@ -1,20 +1,33 @@
 <?php
 
+/*
+    Config générale de l'application.
+
+    Contient :
+    - les chemins  du projet
+    - l'URL
+    - la config de la BDD
+    - les constantes de l'application
+*/
+
+
 // --------------------
 // Chemins du projet
 // --------------------
-
 define('ROOT', dirname(__DIR__));
 
 define('APP_PATH', ROOT . '/app');
 define('MODEL_PATH', APP_PATH . '/models');
 define('VIEW_PATH', APP_PATH . '/views');
 define('CONTROLLER_PATH', APP_PATH . '/controllers');
+
 define('INCLUDE_PATH', ROOT . '/includes');
 define('PUBLIC_PATH', ROOT . '/public');
 
+
 // URL
 define('BASE_URL', '/braveAndSupplyV2/');
+
 
 // BDD
 define('DB_PATH', ROOT . '/config/braveandsupplyv2.db');
@@ -24,6 +37,6 @@ define('DB_PATH', ROOT . '/config/braveandsupplyv2.db');
 // Catégories
 // --------------------
 
-define("CATEGORY_HOMME", 1);
-define("CATEGORY_FEMME", 2);
-define("CATEGORY_KIDS", 3);
+define('CATEGORY_HOMME', 1);
+define('CATEGORY_FEMME', 2);
+define('CATEGORY_KIDS', 3);
