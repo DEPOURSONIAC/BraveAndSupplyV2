70bcebd30722dfb799907ab6e99032a4befb3a07
DEPOUROSNIAC <DEPOURSONIAC.JEAN@outlook.com>
2026-09-12 14:12:58 +0200

    Add files via upload

--- includes/footer.php
+++ includes/footer.php
@@ -1,94 +1,96 @@
 <!-- ***** Footer Start ***** -->
-<footer>
+    <footer>
 
-    <div class="container">
+        <div class="container">
 
-        <div class="row">
+            <div class="row">
 
-            <div class="col-lg-3">
-                <div class="first-item">
+                <div class="col-lg-3">
+                    <div class="first-item">
 
-                    <div class="logo">
-                        <img src="<?= BASE_URL ?>assets/images/logo_site.png" alt="Logo Brave & Supply" width="150">
+                        <div class="logo">
+                            <img src="<?= BASE_URL ?>assets/images/logo_site.png" alt="Logo Brave & Supply" width="150">
+                        </div>
+
+                        <ul>
+                            <li><a href="https://maps.app.goo.gl/aU5PrYLKgkfTEbWP6">Puteaux La Défense 92800 Paris</a></li>
+                            <li><a href="mailto:contact@braveandsupply.com">contact@braveandsupply.com</a></li>
+                            <li><a href="tel:+33612345678">06 12 34 56 78</a></li>
+                        </ul>
+
                     </div>
+                </div>
+
+                <div class="col-lg-3">
+
+                    <h4>Shopping & Categories</h4>
 
                     <ul>
-                        <li><a href="https://maps.app.goo.gl/aU5PrYLKgkfTEbWP6">Puteaux La Défense 92800 Paris</a></li>
-                        <li><a href="mailto:contact@braveandsupply.com">contact@braveandsupply.com</a></li>
-                        <li><a href="tel:+33612345678">06 12 34 56 78</a></li>
+                        <li><a href="<?= BASE_URL ?>?action=categorie&id=1">Hommes</a></li>
+                        <li><a href="<?= BASE_URL ?>?action=categorie&id=2">Femmes</a></li>
+                        <li><a href="<?= BASE_URL ?>?action=categorie&id=3">Enfants</a></li>
                     </ul>
 
                 </div>
-            </div>
 
-            <div class="col-lg-3">
+                <div class="col-lg-3">
 
-                <h4>Shopping & Categories</h4>
-
-                <ul>
-                    <li><a href="<?= BASE_URL ?>?action=categorie&id=1">Hommes</a></li>
-                    <li><a href="<?= BASE_URL ?>?action=categorie&id=2">Femmes</a></li>
-                    <li><a href="<?= BASE_URL ?>?action=categorie&id=3">Enfants</a></li>
-                </ul>
-
-            </div>
-
-            <div class="col-lg-3">
-
-                <h4>Liens utiles</h4>
-
-                <ul>
-                    <li><a href="<?= BASE_URL ?>">Accueil</a></li>
-                    <li><a href="<?= BASE_URL ?>?action=about">À propos</a></li>
-                    <li><a href="<?= BASE_URL ?>?action=contact">Contact</a></li>
-                    <li><a href="<?= BASE_URL ?>?action=mentions">Mentions légales</a></li>
-                </ul>
-
-            </div>
-
-            <div class="col-lg-3">
-
-                <h4>Informations</h4>
-
-                <ul>
-                    <li><a href="<?= BASE_URL ?>?action=infos">Informations</a></li>
-                    <li><a href="<?= BASE_URL ?>?action=cgv">Export CGV</a></li>
-                    <li><a href="<?= BASE_URL ?>?action=reglement">Règlement</a></li>
-                </ul>
-
-            </div>
-
-            <div class="col-lg-12">
-
-                <div class="under-footer">
-
-                    <p>
-                        Copyright © 2026 Brave & Supply
-                        <br>
-                        Design : TemplateMo & DEPOURSONIAC
-                    </p>
+                    <h4>Liens utiles</h4>
 
                     <ul>
-
-                        <li>
-                            <a href="https://www.facebook.com">
-                                <i class="fa fa-facebook"></i>
-                            </a>
-                        </li>
-
-                        <li>
-                            <a href="https://www.x.com">
-                                <i class="fa fa-twitter"></i>
-                            </a>
-                        </li>
-
-                        <li>
-                            <a href="https://www.linkedin.com">
-                                <i class="fa fa-linkedin"></i>
-                            </a>
-                        </li>
-
+                        <li><a href="<?= BASE_URL ?>">Accueil</a></li>
+                        <li><a href="<?= BASE_URL ?>?action=about">À propos</a></li>
+                        <li><a href="<?= BASE_URL ?>?action=contact">Contact</a></li>
+                        <li><a href="<?= BASE_URL ?>?action=mentions">Mentions légales</a></li>
                     </ul>
+
+                </div>
+
+                <div class="col-lg-3">
+
+                    <h4>Informations</h4>
+
+                    <ul>
+                        <li><a href="<?= BASE_URL ?>?action=infos">Informations</a></li>
+                        <li><a href="<?= BASE_URL ?>?action=cgv">Export CGV</a></li>
+                        <li><a href="<?= BASE_URL ?>?action=reglement">Règlement</a></li>
+                    </ul>
+
+                </div>
+
+                <div class="col-lg-12">
+
+                    <div class="under-footer">
+
+                        <p>
+                            Copyright © 2026 Brave & Supply
+                            <br>
+                            Design : TemplateMo & DEPOURSONIAC
+                        </p>
+
+                        <ul>
+
+                            <li>
+                                <a href="https://www.facebook.com">
+                                    <i class="fa fa-facebook"></i>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="https://www.x.com">
+                                    <i class="fa fa-twitter"></i>
+                                </a>
+                            </li>
+
+                            <li>
+                                <a href="https://www.linkedin.com">
+                                    <i class="fa fa-linkedin"></i>
+                                </a>
+                            </li>
+
+                        </ul>
+
+                    </div>
 
                 </div>
 
@@ -96,11 +98,9 @@
 
         </div>
 
-    </div>
+    </footer>
 
-</footer>
-
-    <!-- JS -->
+<!-- JS -->
     <script src="<?= BASE_URL ?>assets/js/jquery-2.1.0.min.js"></script>
     <script src="<?= BASE_URL ?>assets/js/popper.js"></script>
     <script src="<?= BASE_URL ?>assets/js/bootstrap.min.js"></script>
